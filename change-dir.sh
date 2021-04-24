2e941c8912d4413920223c7c85dc632d482f0e9e
Ritik <ritik.linux@gmail.com>
2021-04-24 23:44:36 +0530

    change-dirV1.0

---
+++ change-dir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home
+pwd
+ls
+
+#Do source change-dir.sh to go in that dir
+#Dont Use exit code when using source because if the script is sourced within the current shell, then the current shell is closed
+
