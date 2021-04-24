28a2d116dee1e6993c6dfd6108503e8090de915b
Ritik Gupta <68753687+RG-linux-lover@users.noreply.github.com>
2021-04-24 23:49:08 +0530

    change-dirV1.01

--- change-dir.sh
+++ change-dir.sh
@@ -4,6 +4,6 @@ cd /home
 pwd
 ls
 
-#Do source change-dir.sh to go in that dir
+#Do source change-dir.sh to make this script work and go to variable dir
 #Dont Use exit code when using source because if the script is sourced within the current shell, then the current shell is closed
 
